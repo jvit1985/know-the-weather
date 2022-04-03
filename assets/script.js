1502553eb7a35669db7f303a71b3763b94d8b67a
Justin Vititoe <jvit1985@yahoo.com>
2022-04-03 16:17:04 -0500

    added ability for city name and current forecast to display

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,10 @@ var searchedCityEl = document.querySelector("#searched-city");
 var searchButtonEl = document.querySelector("#search-button");
 var savedCityEl = document.querySelector("#saved-city");
 var currentTempEl = document.querySelector("#current-temp");
+var currentCityEl = document.querySelector("#current-city");
+var currentWindEl = document.querySelector("#current-wind");
+var currentHumidityEl = document.querySelector("#current-humidity");
+var currentUvEl = document.querySelector("#current-uv");
 
 //Get city name from search input from user
 var convertToLatandLon = function(searchedCityEl) {
@@ -12,6 +16,7 @@ var convertToLatandLon = function(searchedCityEl) {
         if(response.ok) {
             response.json().then(function(data) {
                 getCurrentForecast(data[0].lat, data[0].lon)
+                displayCity(data[0].name)
             });
         } else {
             alert("No City Found");
@@ -24,21 +29,28 @@ var convertToLatandLon = function(searchedCityEl) {
 
 
 var getCurrentForecast = function(lat, lon) {
-    console.log(lat, lon);
     var latAndLon = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=85128827aad4fb15f19c4556286ccef6";
 
     fetch (latAndLon).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-               // displayCurrentForecast(data.current.temp)
-                console.log(data.current.temp, data.current.wind_speed, data.current.humidity, data.current.uvi);
-                console.log(data.daily[1].temp.max, data.daily[1].wind_speed, data.daily[1].humidity);
+                console.log(data);
+                //displayForecast(data.daily[i].temp.max, data.daily[i].wind_speed, data.daily[i].humidity);
+                currentTempEl.textContent = "Temp: " + data.daily[0].temp.max + " °F";
+                currentWindEl.textContent = "Wind: " + data.daily[0].wind_speed + " MPH";
+                currentHumidityEl.textContent = "Humidity: " + data.daily[0].humidity + " %";
+                currentUvEl.textContent = "UV Index: " + data.daily[0].uvi;
+               // console.log(data.daily[i].temp.max, data.daily[i].wind_speed, data.daily[i].humidity, data.daily[i].uvi);
             });
         } else {
             alert("No City Found");
         }
     });
 };
+
+var displayCity = function(city) {
+    currentCityEl.textContent = city;
+}
 
 var buttonClickHandler = function(event) {
     event.preventDefault();
@@ -47,25 +59,51 @@ var buttonClickHandler = function(event) {
 
     if (searchedCity) {
         convertToLatandLon(searchedCity);
-        searchedCityEl.value = "";
+        //searchedCityEl.value = "";
         var cityEl = document.createElement("button");
         cityEl.classList = "list-item flex-row justify-space-between align-center btn-block btn-secondary";
         cityEl.setAttribute("btn", searchedCity);
         //append to page
         cityEl.appendChild(savedCityEl);
+        searchedCityEl.value = "";
         
     } else {
         alert("Please enter a valid city");
     }
 };
 
-var displayCurrentForecast = function(currentTemp) {
+var displayForecast = function(currentTemp, currentWindSpeed, currentHumidity) {
     //display current forecast
-    //create and display currentTemp element to page
-    var currentTemp = document.createElement("span");
-    currentTemp.textContent = getCurrentForecast.data.current.temp;
-    currentTemp.append(currentTempEl);
+    // currentTempEl.textContent = "Temp: " + currentTemp;
+    // currentWindEl.textContent = "Wind: " + currentWindSpeed;
+    // currentHumidityEl.textContent = "Humidity: " + currentHumidity;
+    // currentUvEl.textContent = "UV Index: " + currentUvi;
 
+    for (var i = 1; i < 6; i++) {
+
+        var forecastDate = document.createElement("h4");
+        forecastDate.textContent = date + [i];
+
+        var forecastTemp = document.createElement("p");
+        forecastTemp.textContent = "Temp: " + currentTemp[i];
+
+        var forecastWind = document.createElement("p");
+        forecastWind.textContent = "Wind: " + currentWindSpeed[i];
+        
+        var forecastHumidity = document.createElement("p");
+        forecastHumidity.textContent = "Humidity: " + currentHumidity[i];
+
+        // var forecastDate = document.createElement("h4");
+        // forecastDate.textContent = date + [i];
+
+        cardBodyEl.appendChild(forecastTemp);
+        cardBodyEl.appendChild(forecastWind);
+        cardBodyEl.appendChild(forecastHumidity);
+        
+    }
+
+    //create and display currentTemp element to page
+    
     //create and display currentWindSpeed element to page
 
     //create and display currentHumidity element to page
